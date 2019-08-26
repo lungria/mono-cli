f6438dedf3fc31c2d61fdf926f811f76abc9a1ce
Artem <kolomytsev1996@gmail.com>
2019-08-26 22:15:59 +0300

    add mono sdk dep

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lungria/mono-cli
 
 go 1.12
+
+require github.com/lungria/mono v0.0.10
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/lungria/mono"
+)
 
 func main(){
+	mono.Client{}
 	fmt.Print("hello")
 }
