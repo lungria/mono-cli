1a2d9bb55ca631f451db74f087c5ecb62d32756b
Artem <kolomytsev1996@gmail.com>
2019-08-27 01:13:00 +0300

    add basic request for statement export

--- main.go
+++ main.go
@@ -3,9 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/lungria/mono"
+	"log"
+	"os"
+	"time"
 )
 
 func main(){
-	mono.Client{}
-	fmt.Print("hello")
+	token := os.Getenv("MONO_APIKEY")
+	auth := mono.NewPersonalAuth(token)
+	client := mono.New(auth)
+	user, err := client.User()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	now := time.Now().UTC()
+	twoDaysBefore := time.Now().Add(-time.Hour * time.Duration(48))
+
+	statements, err := client.Statement(user.Accounts[0].ID, twoDaysBefore, now)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v", statements)
 }
